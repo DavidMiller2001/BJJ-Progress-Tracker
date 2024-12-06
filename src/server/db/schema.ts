e0fb1a728a91470462911f95e3a487b147d8cfe8
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-12-06 17:09:46 -0500

    db schemas for users and events

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
 
 /**
@@ -10,7 +10,9 @@ import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `bjj-progress-tracker_${name}`);
+export const createTable = sqliteTableCreator(
+  (name) => `bjj-progress-tracker_${name}`,
+);
 
 export const posts = createTable(
   "post",
@@ -21,10 +23,40 @@ export const posts = createTable(
       .default(sql`(unixepoch())`)
       .notNull(),
     updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
+
+// db data for dates w notes and events
+
+export const users = createTable("users", {
+  id: int("id", { mode: "number" }).primaryKey(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  events: many(events),
+}));
+
+export const events = createTable("events", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  authorId: int("author_id", { mode: "number" }).notNull(),
+  title: text("title", { length: 256 }),
+  content: text("content", { length: 256 }),
+  createdAt: int("created_at", { mode: "timestamp" })
+    .default(sql`(unixepoch())`)
+    .notNull(),
+  updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const eventsRelations = relations(events, ({ one }) => ({
+  author: one(users, {
+    fields: [events.authorId],
+    references: [users.id],
+  }),
+}));
