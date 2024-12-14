53e14fe6461a39b45155fe299199adbc76053342
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-12-13 19:56:39 -0500

    make page route dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Container from "~/components/ContainerWithUserData";
 import { getEvents } from "~/server/actions";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const events = await getEvents();
   return (
