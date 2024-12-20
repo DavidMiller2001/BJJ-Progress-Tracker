40fcebf6bf7f4f4a556dbf7c665ac8348444e8bf
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-12-20 17:06:58 -0500

    define new server action for updating existing events

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -4,7 +4,6 @@ import { formSchema } from "~/components/EventForm";
 import { events } from "./db/schema";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { and, eq } from "drizzle-orm";
@@ -46,4 +45,34 @@ export async function deleteEvent(id: number) {
     .where(and(eq(events.id, id), eq(events.authorId, user.userId)));
 
   revalidatePath("/");
+}
+
+export async function updateEvent(
+  id: number,
+  formData: z.infer<typeof formSchema>,
+) {
+  const user = await auth();
+
+  if (!user || !user.userId) {
+    throw new Error("Unauthorized!");
+  }
+
+  const selectedEvent = await db.query.events.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
+
+  if (!selectedEvent) {
+    throw new Error(`Event with id: ${id} does not exist!`);
+  }
+
+  await db
+    .update(events)
+    .set({
+      title: formData.title,
+      content: formData.content,
+      authorId: user.userId,
+      eventDate: formData.date,
+      type: formData.type,
+    })
+    .where(and(eq(events.id, id), eq(events.authorId, user.userId)));
 }
